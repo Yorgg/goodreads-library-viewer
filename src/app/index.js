b8a065432070668a0a1776c42b0942c33382502a
Jon <jyorg@outlook.com>
2016-12-24 01:40:12 +0700

    initial commit -m

---
+++ src/app/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { render } from 'react-dom'
+
+const App = () => {
+  return(
+    <p> Initial Commit </p>
+  )
+}
+
+render(<App />, window.document.getElementById('app'))
